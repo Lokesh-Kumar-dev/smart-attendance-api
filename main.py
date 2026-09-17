a8de0c5ae9dd8a021b2222006f86d49d93ad4f0f
Lokesh-Kumar-dev <plokeshgoud2003@gmail.com>
2026-09-17 20:38:35 +0530

    Refactor comments in main.py for better clarity
    Updated comments for clarity and removed unnecessary descriptors.

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import time
 
 app = FastAPI(title="Smart Attendance API - Lokesh Project")
 
-# Difficult Task - Background Function
 def send_email_task(roll_no: int):
     time.sleep(3)
-    print(f"📧 Background Email sent to Roll No: {roll_no}")
+    print(f" Background Email sent to Roll No: {roll_no}")
 
 @app.get("/")
 def home():
@@ -16,19 +15,17 @@ def home():
 
 @app.post("/add-student")
 def add_student(student: Student):
-    # OOPS + MongoDB together
     attendance_collection.insert_one(student.dict())
     return {"status": "Success", "student": student.get_details()}
 
 @app.get("/students")
 def get_students():
-    # JOIN logic here if you use SQL, for now MongoDB
     students = list(attendance_collection.find({}, {"_id": 0}))
     return {"total": len(students), "data": students}
 
 @app.post("/mark-attendance/{roll_no}")
 def mark_attendance(roll_no: int, background_tasks: BackgroundTasks):
-    # Threading / Background Task - Most Difficult & Useful
+    # Threading 
     background_tasks.add_task(send_email_task, roll_no)
     return {"message": f"Attendance marked for {roll_no}"}
 
