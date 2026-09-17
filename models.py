38424c54cabf69ab9ebfd9509060956d0b1c4cb0
Lokesh-Kumar-dev <plokeshgoud2003@gmail.com>
2026-09-17 20:39:42 +0530

    MyFast api

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
 from pydantic import BaseModel
 
-# OOPS - This is used everywhere
 class Student(BaseModel):
     roll_no: int
     name: str
     branch: str
     city: str = "Machilipatnam"
 
-    # OOPS Method
     def get_details(self):
         return f"{self.roll_no} - {self.name} from {self.branch}"
