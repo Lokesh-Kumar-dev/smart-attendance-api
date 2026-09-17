5098b55d2b5e685134c882583a7096352cac9a2f
Lokesh-Kumar-dev <plokeshgoud2003@gmail.com>
2026-09-17 20:36:15 +0530

    Simplify MongoDB connection print statements

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
-
-# MongoDB to VS Code Python Connection
 try:
     client = MongoClient("mongodb://localhost:27017/")
     db = client["college_db"]
     attendance_collection = db["attendance"]
-    print("✅ MongoDB Connected Successfully!")
+    print("MongoDB Connected Successfully!")
 except Exception as e:
-    print(f"❌ Connection Failed: {e}")
+    print(f"Connection Failed: {e}")
